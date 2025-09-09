feca12a272ceb4ff851f231d841b021c645b97af
Ashish Khanna <shiwangi349@gmail.com>
2025-09-09 12:23:09 +0530

    Final Updates

--- finance_management_system/src/main/java/com/example/finance_management/DataLoader.java
+++ finance_management_system/src/main/java/com/example/finance_management/DataLoader.java
@@ -1,7 +1,6 @@
 package com.example.finance_management;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
